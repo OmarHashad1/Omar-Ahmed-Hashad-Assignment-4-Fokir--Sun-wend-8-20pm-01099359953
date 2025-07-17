a359670468f38ee0fda85e7365c4a5c51a2110d6
OmarHashad <omarhashad88@gmail.com>
2025-07-17 22:08:29 +0300

    Add files via upload

---
+++ app.js
@@ -0,0 +1,7 @@
+  var typed = new Typed(".auto-type", {
+    strings: ["Designer", "Developer", "Freelancer"],
+    typeSpeed: 40,
+    backSpeed: 40,
+    loop: true,
+  });
+  //The video I Watched to make this animation https://www.youtube.com/watch?v=IbGSsy17Ass
